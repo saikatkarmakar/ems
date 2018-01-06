3a8306a1389cd26f69b60132598ee232357cc97c
Saikat Karmakar <sktkarmakar007@gmail.com>
2018-01-06 12:43:09 +0530

    Form validation

--- src/app/employee/employee-create/employee-create.component.ts
+++ src/app/employee/employee-create/employee-create.component.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmployeeService } from '../employee.service';
+
 @Component({
   selector: 'app-employee-create',
   templateUrl: './employee-create.component.html',
-  styleUrls: ['./employee-create.component.css']
+  styleUrls: ['./employee-create.component.css'],
+  providers: [EmployeeService]
 })
 export class EmployeeCreateComponent implements OnInit {
   createEmployee: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private _employeeService: EmployeeService) {
     this.createForm();
   }
 
@@ -17,16 +20,26 @@ export class EmployeeCreateComponent implements OnInit {
 
   createForm() {
     this.createEmployee = this.fb.group({ // <-- the parent FormGroup
-      name: ['', Validators.required ],
-      email: ['', Validators.required],
-      address: this.fb.group({ // <-- the child FormGroup
+      firstName: ['', Validators.required ],
+      lastName: ['', Validators.required ],
+      emailId: ['', [Validators.required, Validators.email]],
+      /* address: this.fb.group({ // <-- the child FormGroup
         street: '',
         city: '',
         state: '',
         zip: ''
       }),
-      status: '',
+      status: '', */
     });
+  };
+
+  create(){
+    var req = this.createEmployee.value;
+    this._employeeService.create(req);
+  }
+
+  logThis(){
+    console.log(this.createEmployee);
   }
 
 }
